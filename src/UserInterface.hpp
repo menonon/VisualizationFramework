a7ce189b90017ffd88615a22a4f746801faf8dd1
MENO Daiusuke <menonon@vizlab.cs.kobe-u.ac.jp>
2011-09-26 15:40:38 +0900

    change name of *.h to *.hpp. and add UserInterface

---
+++ src/UserInterface.hpp
@@ -0,0 +1,14 @@
+
+/**
+* UserInterface
+* @author MENO Daisuke (menonon@vizlab.cs.kobe-u.ac.jp)
+* @version developping
+*/
+
+class UserInterface{
+
+public:
+
+private:
+
+};
---
+++ src/VisualizeMethod.hpp
@@ -0,0 +1,26 @@
+
+/**
+* VisualizeMethod
+* @author MENO Daisuke (menonon@vizlab.cs.kobe-u.ac.jp)
+* @version developping
+*/
+
+
+class VisualizeMethod{
+
+ public:
+  void draw();
+  void init();
+  void contextInit();
+  void preFrame();
+  void intraFrame();
+  void postFrame();
+
+
+
+ private:
+
+
+
+
+};
