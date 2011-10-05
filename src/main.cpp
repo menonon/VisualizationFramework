bdac6154cf5f3440eed19e6dd82c048b6298e62c
MENO Daisuke <menonon@vizlab.cs.kobe-u.ac.jp>
2011-10-05 18:32:07 +0900

    main.cpp complete

--- src/main.cpp
+++ src/main.cpp
@@ -7,3 +7,24 @@
 #include <cstdlib>
 
 #include <vrj/Kernel/Kernel.h>
+
+
+int main(int argc, char* argv){
+
+  Kernel* kernel = Kernel::instance();
+  App* app = new App();
+  kernel->init(argc, argv);
+
+  for(int i=1;i<argc;1++){
+    kernel->loadConfigFile(argv[i]);
+  }
+
+  kernel->start();
+  kernel->setApplication(app);
+  kernel->waitForKernelStop();
+
+  delete app;
+  return EXIT_SUCCESS
+
+
+}
